420ae7b6cad7a7236e02085c2ab510bbc0d79631
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-14 23:17:46 +0300

    YuzdeBulma

---
+++ YuzdeBulma/YuzdeBulma/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YuzdeBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sayi, yuzde, sonuc;
+            Console.Write("Sayıyı Girin : ");
+            sayi = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Yüzdeyi Girin : ");
+            yuzde = Convert.ToDouble(Console.ReadLine());
+            sonuc = sayi * yuzde / 100;
+            Console.WriteLine("Girilen sayının % {0}' i : {1}", yuzde, sonuc);
+            Console.ReadKey();
+        }
+    }
+}
